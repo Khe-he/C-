3d1f6b512e812e660093e9b1b3403c630484ff5a
Khe-he <sipkoantonz@gmail.com>
2018-06-21 00:07:45 +0700

    Initial commit

---
+++ bubble sort/bubble sort console/bubble sort console/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubble_sort_console
+{
+    class Program
+    {
+       void bubble_sort(string[] array_one)
+        {
+            for(int i=0;i<array_one.Length-1;i++)
+            {
+
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            string[] one = new string[5];
+            int size = one.Length;
+            int lenght = 5;
+           /* for (int i=0;i<5;i++)//заполнение рандомом массива
+            {
+                for (int n = 0; n < 5; n++)
+                {
+                    one[i] =one[i]+Convert.ToChar(rnd.Next('a', 'a' + 27)).ToString();
+                }
+            }*/
+            one[0]="aaaaa";
+            one[1] = "ccccc";
+            one[2] = "eeeee";
+            one[3] = "bbbbb";
+            one[4] = "ddddd";
+
+            Console.WriteLine("До сортировки:");
+            for (int i = 0; i < 5; i++)//вывод массива
+            {
+                Console.WriteLine(one[i]);
+                
+            }
+            Console.WriteLine("Кол-во строк "+size+" Длина строки "+lenght);
+            string x = "";
+            for(int i=lenght-1;i>0;i--)
+             {
+                int summ_l = 0, summ_pre = 0;
+                byte[] res_last = System.Text.Encoding.Default.GetBytes(one[i]);//код символов последней строчки
+                byte[] res_pre = System.Text.Encoding.Default.GetBytes(one[i - 1]);//код символов predпоследней строчки
+                for (int e = size-1; e > 0; e--)
+                {
+                    summ_l = summ_l + res_last[e];
+                    summ_pre = summ_pre + res_pre[e];
+                   
+                }
+                Console.WriteLine(summ_l + " " + summ_pre);
+                 
+             }
+            for(int i=0;i<size;i++)
+            {
+               // Console.WriteLine(one[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ bubble sort/bubble sort console/bubble sort console/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("bubble sort console")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bubble sort console")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b74e5bce-0eed-455e-9919-d2f0af88a96a")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
